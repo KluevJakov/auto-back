d43fafd5fefe71487f925ccaf77ec8f2db94f6f7
loo9y <kluevja@gmail.com>
2024-06-14 05:56:07 +0400

    upd

--- src/main/java/ru/jafix/auto/controller/OrdersController.java
+++ src/main/java/ru/jafix/auto/controller/OrdersController.java
@@ -1,22 +1,17 @@
 package ru.jafix.auto.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import ru.jafix.auto.entity.FormRequestDTO;
 import ru.jafix.auto.entity.Order;
 import ru.jafix.auto.entity.User;
 import ru.jafix.auto.repository.OrderRepository;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @RestController
 public class OrdersController {
@@ -38,4 +33,12 @@ public class OrdersController {
         return ResponseEntity.ok(orders);
     }
 
+    @PutMapping("/orders/{id}")
+    public ResponseEntity<String> processOrder(@PathVariable UUID id, @RequestBody Map<String, String> request) {
+        String status = request.get("status");
+        Order order = orderRepository.findById(id).get();
+        order.setStatus(status);
+        orderRepository.save(order);
+        return ResponseEntity.ok("Order processed successfully");
+    }
 }
